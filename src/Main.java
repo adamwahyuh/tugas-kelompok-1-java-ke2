25f30c4f7b40f7458bc56fa9950d7ee827229fb9
adamwahyuh <adamwahyu875@gmail.com>
2025-03-20 18:11:47 +0700

    init

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public  static void main(String[] args){
+
+    }
+}
+
+class AngkaKeHuruf{
+    public  static void main(String[] args){
+
+    }
+}
+class PositifNegatifNol{
+    public  static void main(String[] args){
+
+    }
+}
+class GenapAtauGanjil{
+    public  static void main(String[] args){
+
+    }
+}
+class KonversiAngkaKeHuruf{
+    public static void main(String[] args){
+        
+    }
+}
