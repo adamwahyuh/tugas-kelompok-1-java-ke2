e79658ccd9dfa39937881df32e6b1a51cbc0c2d5
adamwahyuh <adamwahyu875@gmail.com>
2025-03-20 19:09:38 +0700

    re-init

--- src/Main.java
+++ src/Main.java
@@ -57,17 +57,7 @@ class PositifNegatifNol{
 }
 class GenapAtauGanjil{
     public  static void main(String[] args){
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Masukkan angka: ");
-        int angka = input.nextInt();
-
-        if (angka % 2 == 0) {
-            System.out.println(angka + " adalah bilangan genap.");
-        } else {
-            System.out.println(angka + " adalah bilangan ganjil.");
-        }
-    }
     }
 }
 class KonversiAngkaKeHuruf{
